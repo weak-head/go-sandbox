c073f90209853472ce0bf49a9a53b49462d99e55
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-08 08:56:37 -0700

    Basic dirty channel multiplexing

--- pkg/patterns/multiplexing.go
+++ pkg/patterns/multiplexing.go
@@ -7,14 +7,22 @@ import (
 // FanIn dirty way to multiplex two channels
 func FanIn(input1, input2 <-chan string) <-chan string {
 	c := make(chan string)
-	go func() { c <- <-input1 }()
-	go func() { c <- <-input2 }()
+	go func() {
+		for {
+			c <- <-input1
+		}
+	}()
+	go func() {
+		for {
+			c <- <-input2
+		}
+	}()
 	return c
 }
 
 // Usage is an example if using simple channel mutliplexing
 func FanInUsage() {
-	c := FanIn(Generator("Hey"), Generator("How"))
+	c := FanIn(Generator("Hey"), Generator("Wow Wow"))
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Received: %s\n", <-c)
 	}
