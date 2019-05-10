eaf9857f1cc8e7e9163639f06712853f4ebcc3f7
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-10 08:35:00 -0700

    Sequential search example

---
+++ cmd/example/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	ex "gobox/pkg/example"
+)
+
+func main() {
+	ex.DoSearch()
+}
--- pkg/example/googlesearch.go
+++ pkg/example/googlesearch.go
@@ -3,7 +3,15 @@ package example
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
+)
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+	All   = []Search{Web, Image, Video}
 )
 
 // Result is a search result
@@ -18,4 +26,29 @@ func fakeSearch(kind string) Search {
 		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 		return Result(fmt.Sprintf("%s result for %s", kind, query))
 	}
+}
+
+// Google1 -> sequential search one by one
+func Google1(query string) (results []Result) {
+	for _, s := range All {
+		results = append(results, s(query))
+	}
+	return
+}
+
+// DoSearch starts the search and prints out the results
+func DoSearch() {
+	rand.Seed(time.Now().UnixNano())
+
+	start := time.Now()
+	results := Google1("golang")
+	elapsed := time.Since(start)
+
+	// fmt.Printf("%#v\n", results)
+	var strs []string
+	for _, r := range results {
+		strs = append(strs, string(r))
+	}
+	fmt.Println(strings.Join(strs, "\n"))
+	fmt.Println(elapsed)
 }
