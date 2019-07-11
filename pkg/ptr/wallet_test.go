64e07bf4e96c7026ccbcf64850bc4d7a26c32e9a
Oleksandr Zinchenko <zinchenko@live.com>
2019-07-10 18:01:06 -0700

    Errors and pointers

---
+++ pkg/ptr/wallet_test.go
@@ -0,0 +1,50 @@
+package ptr
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(15))
+
+		assertBalance(t, wallet, Bitcoin(5))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	balance := wallet.Balance()
+	if balance != want {
+		t.Errorf("got %s want %s", balance, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
