30b5ad178ef8d13aa50d837f7dffaabf30758acb
Oleksandr Zinchenko <zinchenko@live.com>
2019-07-16 11:55:55 -0700

    Smart mock

--- pkg/mocking/mocking_test.go
+++ pkg/mocking/mocking_test.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+)
+
+const (
+	write = "write"
+	sleep = "sleep"
 )
 
 type SpySleeper struct {
 	Calls int
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func TestCountdown(t *testing.T) {
@@ -33,6 +52,26 @@ Go!`
 
 		if sleeper.Calls != 4 {
 			t.Errorf("not enough calls to sleeper, want 4 got %d", sleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		sleeper := &CountdownOperationsSpy{}
+		Countdown(sleeper, sleeper)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, sleeper.Calls) {
+			t.Errorf("wanted calls %v got %v", want, sleeper.Calls)
 		}
 	})
 }
