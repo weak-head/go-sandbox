86c76e6b93b8bad0f49a0a36bd87c2763147c0b2
Oleksandr Zinchenko <zinchenko@live.com>
2019-07-17 23:36:01 -0700

    Reflection, walk

--- pkg/reflection/reflection.go
+++ pkg/reflection/reflection.go
@@ -2,8 +2,37 @@ package reflection
 
 import "reflect"
 
-func walk(st interface{}, fn func(s string)) {
-	val := reflect.ValueOf(st)
-	field := val.Field(0)
-	fn(field.String())
+func walk(x interface{}, fn func(s string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
--- pkg/reflection/reflection_test.go
+++ pkg/reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	t.Run("fixed test cases", func(t *testing.T) {
@@ -17,6 +27,54 @@ func TestWalk(t *testing.T) {
 				testName: "single string field",
 				input:    struct{ Name string }{"Chris"},
 				calls:    []string{"Chris"},
+			},
+			{
+				testName: "struct with two string fields",
+				input: struct {
+					Name string
+					City string
+				}{Name: "Chris", City: "London"},
+				calls: []string{"Chris", "London"},
+			},
+			{
+				testName: "struct with string and int",
+				input: struct {
+					Name string
+					Age  int
+				}{Name: "Chris", Age: 21},
+				calls: []string{"Chris"},
+			},
+			{
+				testName: "nested struct",
+				input: Person{
+					Name:    "Chris",
+					Profile: Profile{Age: 18, City: "London"},
+				},
+				calls: []string{"Chris", "London"},
+			},
+			{
+				testName: "pointer to things",
+				input: &Person{
+					Name:    "Chris",
+					Profile: Profile{Age: 18, City: "London"},
+				},
+				calls: []string{"Chris", "London"},
+			},
+			{
+				testName: "slices",
+				input: []Profile{
+					{18, "London"},
+					{23, "Berlin"},
+				},
+				calls: []string{"London", "Berlin"},
+			},
+			{
+				testName: "arrays",
+				input: [2]Profile{
+					{18, "London"},
+					{23, "Berlin"},
+				},
+				calls: []string{"London", "Berlin"},
 			},
 		}
 
@@ -32,4 +90,33 @@ func TestWalk(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"foo": "bar",
+			"boz": "baz",
+		}
+
+		var got []string
+		walk(aMap, func(s string) {
+			got = append(got, s)
+		})
+
+		assertContains(t, got, "bar")
+		assertContains(t, got, "baz")
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+			break
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
+	}
 }
